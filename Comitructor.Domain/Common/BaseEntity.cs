ffa985569f04450ec23b93bd58b1369eb718f9fa
kcepeda <slenderstalin@hotmail.com>
2026-05-16 21:27:47 -0500

    feat: Creacion de entidad base

---
+++ Comitructor.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Comitructor.Domain.Common
+{
+    public abstract class BaseEntity<TKey> : IAuditableEntity
+    {
+        public TKey Id { get; set; } = default!;
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public int? DeletedBy { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Comitructor.Domain/Common/IAuditableEntity.cs
@@ -0,0 +1,29 @@
+﻿namespace Comitructor.Domain.Common
+{
+    /// <summary>
+    /// Contrato unificado para la auditoría y trazabilidad de entidades.
+    /// Incluye soporte nativo para Soft Delete y control de modificaciones.
+    /// </summary>
+    public interface IAuditableEntity
+    {
+        // --- Sección de Creación ---
+        /// <summary> Identificador del usuario que creó el registro. </summary>
+        public int? CreatedBy { get; set; }
+        /// <summary> Fecha y hora de creación. </summary>
+        public DateTime? CreatedDate { get; set; }
+
+        // --- Sección de Modificación ---
+        /// <summary> Identificador del último usuario que editó el registro. </summary>
+        public int? LastModifiedBy { get; set; }
+        /// <summary> Fecha y hora de la última actualización. </summary>
+        public DateTime? LastModifiedDate { get; set; }
+
+        // --- Sección de Borrado Lógico (Soft Delete) ---
+        /// <summary> Identificador del usuario que eliminó el registro. </summary>
+        public int? DeletedBy { get; set; }
+        /// <summary> Fecha y hora de la eliminación lógica. </summary>
+        public DateTime? DeletedDate { get; set; }
+        /// <summary> Indica si el registro está marcado como eliminado (borrado lógico). </summary>
+        public bool IsDeleted { get; set; }
+    }
+}
