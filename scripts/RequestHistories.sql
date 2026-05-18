902b8feae90b88e3f5612b20d54c78eef3dda774
kcepeda <slenderstalin@hotmail.com>
2026-05-18 03:52:05 -0500

    feat: se agrega los scripts de sql server

---
+++ scripts/RequestHistories.sql
@@ -0,0 +1,12 @@
+INSERT INTO [RequestHistories] ([Id], [RequestId], [PreviousStatus], [NewStatus], [ChangeReason], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (1, 16, N'New', N'InProgress', N'Por ahora estamos validando que si las maquinas tienen alguna manera de extenderles mas la vida util', NULL, NULL, NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [RequestHistories] ([Id], [RequestId], [PreviousStatus], [NewStatus], [ChangeReason], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (2, 5, N'New', N'InProgress', N'Se esta limpiando el sanitario lo mas rapido posible', NULL, NULL, NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [RequestHistories] ([Id], [RequestId], [PreviousStatus], [NewStatus], [ChangeReason], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (3, 16, N'InProgress', N'Closed', N'Ya esta limpio', NULL, NULL, NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [RequestHistories] ([Id], [RequestId], [PreviousStatus], [NewStatus], [ChangeReason], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (4, 16, N'Closed', N'Cancelled', N'asdadadadasdasda', NULL, NULL, NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [RequestHistories] ([Id], [RequestId], [PreviousStatus], [NewStatus], [ChangeReason], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (5, 1, N'New', N'InProgress', N'Estamos en progreso con las reparaciones', NULL, NULL, NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [RequestHistories] ([Id], [RequestId], [PreviousStatus], [NewStatus], [ChangeReason], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (6, 16, N'Cancelled', N'Cancelled', N'Prueba parte 4', NULL, NULL, NULL, NULL, NULL, NULL, '0');
+GO
---
+++ scripts/Users.sql
@@ -0,0 +1,14 @@
+INSERT INTO [Users] ([Id], [Username], [Password], [Role], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (1, N'admin', N'$2a$12$coCqS.0LdOugpt75mQFURu5W8nLADt/0BaHZxOj9UjnQNAPs.rkyS', N'Administrator', NULL, '2026-05-17 06:39:59.3670000', NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [Users] ([Id], [Username], [Password], [Role], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (2, N'operador', N'$2a$12$3C8s98yQEQ/X8/if24TQ/uyMCZLYMRWLHOtXUw/.ERO8UHzh3nbJ2', N'Operator', NULL, '2026-05-17 06:39:59.3674711', NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [Users] ([Id], [Username], [Password], [Role], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (3, N'kcepeda', N'$2a$12$bthH4wbHfm6yYpp.z8dd0eygJsE702Wa0T8wEjkIVWeIgqoAkqYmS', N'Operator', NULL, '2026-05-17 06:48:34.3820040', NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [Users] ([Id], [Username], [Password], [Role], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (4, N'acepeda', N'$2a$12$J8k0tVrxtEGWlqxVF9yFceaqg8CtdLjvhesG5d7SxHr49wOlVmTPa', N'Operator', NULL, '2026-05-17 06:49:01.2242314', NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [Users] ([Id], [Username], [Password], [Role], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (5, N'cyborgk27', N'$2a$12$y4Yc4S3edRf6hklv1LQ68OV.lTQa1kDLSym7FiHzRQ32G2FtWwmZe', N'Operator', NULL, '2026-05-17 06:49:30.6040185', NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [Users] ([Id], [Username], [Password], [Role], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (6, N'alexkiller0408', N'$2a$12$cmcM58wUjNj16fPGu8u0nuQPCiMFRb9khiHTBo8TR.he6OBOOQYXK', N'Operator', NULL, '2026-05-17 06:50:26.8815351', NULL, NULL, NULL, NULL, '0');
+GO
+INSERT INTO [Users] ([Id], [Username], [Password], [Role], [CreatedBy], [CreatedDate], [LastModifiedBy], [LastModifiedDate], [DeletedBy], [DeletedDate], [IsDeleted]) VALUES (7, N'lumenvita', N'$2a$12$wCsVNeUfoJXaYD.HYDgweOBDXFKpjAhRVtuqfr8ySAKXbcZuVMjC.', N'Operator', NULL, '2026-05-17 06:53:33.1326163', NULL, NULL, NULL, NULL, '0');
+GO
